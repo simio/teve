0b06ffa360b3f7f4bf04de40307d65487109e4e3
Jesper Räftegård <jesper@huggpunkt.org>
2012-09-13 02:27:28 +0200

    Oops

--- intarweb-hack.scm
+++ intarweb-hack.scm
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2012 Jesper Raftegard <jeser@huggpunkt.org>
+;;; Copyright (c) 2012 Jesper Raftegard <jesper@huggpunkt.org>
 ;;;
 ;;; Permission to use, copy, modify, and distribute this software for any
 ;;; purpose with or without fee is hereby granted, provided that the above
