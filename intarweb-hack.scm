e8832ab771e08d7588ad83f02481d30587d8341c
Jesper Räftegård <jesper@huggpunkt.org>
2012-09-15 00:30:16 +0200

    add missing require-extension

--- intarweb-hack.scm
+++ intarweb-hack.scm
@@ -12,7 +12,7 @@
 ;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 ;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-(require-extension intarweb srfi-13)
+(require-extension intarweb srfi-13 uri-common)
 
 ;;; Akamai is emotionally unstable and needs special treatment.
 ;;; The http servers might choke on url:s in GET requests containing
