ee71bc7ae941ae247a652d376897dbf44d4d6389
Jesper Räftegård <jesper@huggpunkt.org>
2012-10-12 18:26:53 +0200

    Add missing copyright/license headers

--- tests/test-apple-hls.scm
+++ tests/test-apple-hls.scm
@@ -13,21 +13,7 @@
  | OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  |#
 
-(require-extension srfi-1)
+(use srfi-13 srfi-14 srfi-78)
 
-;;; The scrape-list is a list of all url->video procedures
-(define *scraper-list* '())
-
-;;; Allow "plugins" in sites/ to add procedures to the scraper-list
-(define (add-scraper procedure)
-  (set! *scraper-list* (cons procedure *scraper-list*)))
-
-(include "sites/svt.scm")
-(include "sites/tv4.scm")
-
-;;; Dispatcher
-;;; Each url->videos procedure is applied to the url. The resulting
-;;; videos are appended into a video-list, which is returned.
-(define (url->videos url)
-  (delete-duplicates
-   (apply append (filter-map (lambda (u->v) (u->v url)) *scraper-list*))))
+(include "tests/global.scm")
+(include "apple-hls.scm")
