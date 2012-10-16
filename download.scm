8a9bee2c0dc1da932853aa0c86059465bc0b73c2
Jesper Räftegård <jesper@huggpunkt.org>
2012-10-17 01:02:19 +0200

    Modularise download command generator

--- download.scm
+++ download.scm
@@ -15,40 +15,64 @@
 
 (include "video.scm")
 
-;;; Produce download(/playback/tee-playback) commands for the shell
+(define (stream->curl/make-command stream outfile)
+  (conc "curl \\" #\newline
+        "    -L \\" #\newline
+        "    -o " (shell-escape outfile) " \\" #\newline
+        "    " (shell-escape (stream-ref 'url stream))))
+
+(define (stream->rtmpdump/make-command stream outfile)
+  (conc "rtmpdump \\" #\newline
+        "    -r " (shell-escape (stream-ref 'url stream)) " \\" #\newline
+        "    -o " (shell-escape outfile) " \\" #\newline
+        (if (stream-ref 'swf-player stream)
+            (conc "    -W " (shell-escape (stream-ref 'swf-player stream))
+                  " \\" #\newline)
+            "")
+        (if (stream-ref 'live stream)
+            (conc "    -v \\" #\newline)
+            "")))
+
+(define (stream->ffmpeg/make-command stream outfile)
+  (conc "ffmpeg \\" #\newline
+        "    -i " (shell-escape (stream-ref 'url stream)) " \\" #\newline
+        "    -acodec copy -vcodec copy \\" #\newline
+        (if (stream-ref 'ffmpeg-parameters stream)
+            (conc "    " (stream-ref 'ffmpeg-parameters stream)
+                  " \\" #\newline)
+            "")
+        (shell-escape outfile)))
+
+(define (stream->adobehds.php/make-command stream outfile)
+  (conc "php AdobeHDS.php \\" #\newline
+        "    --manifest " (shell-escape (stream-ref 'url stream))
+        " \\" #\newline
+        (if (stream-ref 'adobe.hds-parameters stream)
+            (conc "    " (stream-ref 'adobe.hds-parameters stream)
+                  " \\" #\newline)
+            "")
+        "    --outfile " (shell-escape outfile)))
+
+(define (stream->mplayer/make-command stream outfile)
+  (conc "mplayer \\" #\newline
+        "    -dumpstream -dumpfile " (shell-escape outfile)
+        " \\" #\newline
+        (if (stream-ref 'mplayer-parameters stream)
+            (conc "    " (stream-ref 'mplayer-parameters stream)
+                  " \\" #\newline)
+            "")
+        (shell-escape (stream-ref 'url stream))))
+
 (define (stream->download-command stream outfile)
   (case (stream-ref 'stream-type stream)
     ((hds)
-     (conc "php AdobeHDS.php \\" #\newline
-           "    --manifest " (shell-escape (stream-ref 'url stream))
-           " \\" #\newline
-           "    --debug --outfile " (shell-escape outfile) ".flv"))
+     (stream->adobehds.php/make-command stream outfile))
     ((hls)
-     (conc "ffmpeg \\" #\newline
-           "    -i " (shell-escape (stream-ref 'url stream)) " \\" #\newline
-           "    -acodec copy -vcodec copy -absf aac_adtstoasc \\" #\newline
-           "    " (shell-escape outfile) ".mp4"))
+     (stream->ffmpeg/make-command stream outfile))
     ((rtmp)
-     (conc "rtmpdump \\" #\newline
-           "    -r " (shell-escape (stream-ref 'url stream)) " \\" #\newline
-           "    -W " (shell-escape (stream-ref 'swf-player stream))
-           " \\" #\newline
-           (if (stream-ref 'live stream)
-               (conc "    -v \\" #\newline)
-               "")
-           "    -o " (shell-escape outfile) ".flv"))
+     (stream->rtmpdump/make-command stream outfile))
     ((http wmv)
-     (conc "curl \\" #\newline
-           "    -Lo " (shell-escape outfile)
-           (if (eqv? 'http (stream-ref 'stream-type stream))
-               ".flv"
-               ".wmv") " \\" #\newline
-               "    " (shell-escape (stream-ref 'url stream))))
+     (stream->curl/make-command stream outfile))
     ((mms rtsp)
-     (conc "mplayer \\" #\newline
-           "    -dumpstream -dumpfile " (shell-escape outfile)
-           (if (eqv? 'mms (stream-ref 'stream-type stream)) ".wmv" ".mp4")
-           " \\" #\newline
-           "    " (shell-escape (stream-ref 'url stream))))
-    (else
-     #f)))
+     (stream->mplayer/make-command stream outfile))
+    (else #f)))
