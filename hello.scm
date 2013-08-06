simio/teve

commit 460fc328ad4e6ccde4b53b4024baa4bc04f385a9

Convert talk-is-cheap into a module, stupidly named hello.

hello.scm
@@ -1,4 +1,4 @@
-#| Copyright (c) 2012 Jesper Raftegard <jesper@huggpunkt.org>
+#| Copyright (c) 2012, 2013 Jesper Raftegard <jesper@huggpunkt.org>
  | 
  | Permission to use, copy, modify, and distribute this software for any
  | purpose with or without fee is hereby granted, provided that the above
@@ -13,9 +13,16 @@
  | OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  |#
 
-(use srfi-1)
+(module hello *
+(import scheme chicken data-structures srfi-1 extras)
 
-(define talk-prints-debug-messages #f)
+(define print-debug-messages?
+  (let ((print-them #f))
+    (lambda args
+      (if (< 0 (length args))
+          (set! print-them (car args)))
+      print-them)))
+
 (define current-debug-port (current-error-port))
 
 ;;; Print stuff. If stuff consists entirely of strings, numbers and chars,
@@ -51,7 +58,7 @@
 (define-syntax debug-is-cheap
   (syntax-rules (nl: prepend: mapper:)
     ((debug-is-cheap nl: newline prepend: prepend mapper: mapper stuff ...)
-     (if talk-prints-debug-messages
+     (if print-debug-messages?
          (talk-is-cheap current-debug-port
                         (conc prepend ": ")
                         (if newline #\newline "")	; append
@@ -98,3 +105,4 @@
     ((debug* stuff ...)
      (debug-is-cheap nl: #f stuff ...))))
 
+)

parse-flags.scm
@@ -20,7 +20,7 @@
                           "Download stream (default or specified with -i)"
                           (*cfg* set: #t 'operators 'download?))
         (args:make-option (v) #:none "Be verbose"
-                          (set! talk-prints-debug-messages #t)
+                          (print-debug-messages? #t)
                           (debug "Verbose mode."))
         (args:make-option (o) (required: "filename")
                           "Set output filename"

teve.scm
@@ -16,12 +16,14 @@
 (define program-display-name "teve")
 (define program-version "0.2.2")
 
+
 ;; Keep this here to avoid reimporting uri-common stuff
 (include "intarweb-hack.scm")	; loads uri-common; don't load it elsewhere
 
 (include "platform.scm")
 
-(include "talk-is-cheap.scm")
+(include "hello.scm")
+(import hello)
 (include "misc-helpers.scm")
 
 (include "dot-locking.scm")

commit 0916b74d1c7e886666e3213aa8bd14a72ff4b3a8

Fix bug introduced in 460fc328ad4e6ccde4b53b4024baa4bc04f385a9

hello.scm
@@ -58,7 +58,7 @@
 (define-syntax debug-is-cheap
   (syntax-rules (nl: prepend: mapper:)
     ((debug-is-cheap nl: newline prepend: prepend mapper: mapper stuff ...)
-     (if print-debug-messages?
+     (if (print-debug-messages?)
          (talk-is-cheap current-debug-port
                         (conc prepend ": ")
                         (if newline #\newline "")	; append