ba8173d941d57091aefabf3be15252f3ecf6737a
Jesper Räftegård <jesper@huggpunkt.org>
2013-08-06 18:09:03 +0200

    Sort arg opts in alphabetical order and simplify layout

--- parse-flags.scm
+++ parse-flags.scm
@@ -1,67 +1,68 @@
 #| Copyright (c) 2012, 2013 Jesper Raftegard <jesper@huggpunkt.org>
- |
- | Permission to use, copy, modify, and distribute this software for any
- | purpose with or without fee is hereby granted, provided that the above
- | copyright notice and this permission notice appear in all copies.
- |
- | THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- | WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- | MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- | ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- | WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- | ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- | OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- |#
+|
+| Permission to use, copy, modify, and distribute this software for any
+| purpose with or without fee is hereby granted, provided that the above
+| copyright notice and this permission notice appear in all copies.
+|
+| THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+| WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+| MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+| ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+| WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+| ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+| OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+|#
 
 (require-extension args)
 
 (args:width 20)
 
 (define opts
-  (list (args:make-option (d) #:none
-                          "Download stream (default or specified with -i)"
-                          (*cfg* set: #t 'operators 'download?))
-        (args:make-option (v) #:none "Be verbose"
-                          (print-debug-messages? #t))
-        (args:make-option (c) (optional: "type")
-                          (conc "Machine-readable output. "
-                                "Different formats are available:" #\newline
-                                (make-string 21 #\space)
-                                " -c, -cs         Scheme data" #\newline
-                                (make-string 21 #\space)
-                                " -cj             JSON")
-                          ;; Set default to s
-                          (let ((format (if (string? arg)
-                                            (string->symbol arg)
-                                            's)))
-                            (case format
-                              ((s)
-                               (*cfg* set: 'scheme 'operators 'machine-output))
-                              ((j)
-                               (*cfg* set: 'json 'operators 'machine-output))
-                              ((t)
-                               (*cfg* set: 'grep 'operators 'machine-output))
-                              (else
-                               (stderr "Unknown stdout output format: " arg)
-                               (exit 1)))
-                            (debug "stdout set to "
-                                   (*cfg* 'operators 'machine-output) format)))
-        (args:make-option (o) (required: "filename")
-                          "Set output filename"
-                          (*cfg* set: arg 'operators 'output-filename)
-                          (debug (conc
-                                  "Saving to \""
-                                  (*cfg* 'operators 'output-filename)
-                                  "\"")))
-;;        (args:make-option (p) #:none
-;;                          (conc "Play stream (default or specified with -i)")
-;;                          (*cfg* set: #t 'operators 'play?))
-        (args:make-option (l) #:none "List all streams (or specified with -i)"
-                          (*cfg* set: #t 'operators 'list?))
-        (args:make-option (i) (required: "id") "Stream #id to use"
-                          (*cfg* set: (string->number arg)
-                                 'operators 'stream-id))
-        (args:make-option (h) #:none "Display this text" (usage))))
+  (list
+   (args:make-option (c) (optional: "type")
+                     (conc "Machine-readable output. "
+                           "Different types are available:" #\newline
+                           (make-string 21 #\space)
+                           " s  Scheme data [default]" #\newline
+                           (make-string 21 #\space)
+                           " j  JSON")
+                     ;; Set default to s
+                     (let ((format (if (string? arg)
+                                       (string->symbol arg)
+                                       's)))
+                       (case format
+                         ((s)
+                          (*cfg* set: 'scheme 'operators 'machine-output))
+                         ((j)
+                          (*cfg* set: 'json 'operators 'machine-output))
+                         ((t)
+                          (*cfg* set: 'grep 'operators 'machine-output))
+                         (else
+                          (stderr "Unknown stdout output format: " arg)
+                          (exit 1)))
+                       (debug "stdout set to "
+                              (*cfg* 'operators 'machine-output) format)))
+   (args:make-option (d) #:none
+                     "Download stream (default or specified with -i)"
+                     (*cfg* set: #t 'operators 'download?))
+   (args:make-option (h) #:none "Display this text" (usage))
+   (args:make-option (i) (required: "id") "Stream #id to use"
+                     (*cfg* set: (string->number arg)
+                            'operators 'stream-id))
+   (args:make-option (l) #:none "List all streams (or specified with -i)"
+                     (*cfg* set: #t 'operators 'list?))
+   (args:make-option (o) (required: "filename")
+                     "Set output filename"
+                     (*cfg* set: arg 'operators 'output-filename)
+                     (debug (conc
+                             "Saving to \""
+                             (*cfg* 'operators 'output-filename)
+                             "\"")))
+   ;;(args:make-option (p) #:none
+   ;;                  (conc "Play stream (default or specified with -i)")
+   ;;                  (*cfg* set: #t 'operators 'play?))
+   (args:make-option (v) #:none "Be verbose"
+                     (print-debug-messages? #t))))
 
 (define (usage)
   (with-output-to-port (current-error-port)
