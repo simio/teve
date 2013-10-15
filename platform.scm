f21897f07cd68ca8925a7e566f69d4d1f1387248
Jesper Räftegård <jesper@huggpunkt.org>
2013-10-15 12:53:36 +0200

    better windows paths

--- platform.scm
+++ platform.scm
@@ -16,25 +16,48 @@
 
 (require-extension miscmacros)
 
+(define (platform-dir-slashes str)
+  (and (string? str)
+       (if (eq? (software-type) 'windows)
+	   (string-replace-every "/" "\\" str)
+	   (string-replace-every "\\" "/" str))))
+
 (define (make-platform)
-  (let* ((program-filename (let ((actual (pathname-file (program-name))))
+  (let* ((nix/win (lambda (nix win)
+		    (if (eq? (software-type) 'windows)
+			win
+			nix)))
+	 (program-filename (let ((actual (pathname-file (program-name))))
                              (if (equal? "csi" actual) "teve" actual)))
-         (home-dir (get-environment-variable "HOME"))
-	 (user-data-dir (conc home-dir "/." program-filename))
-         (system-config-file (if* (file-exists?
-                                   (conc "/etc/" program-filename ".conf"))
+         (home-dir (nix/win (get-environment-variable "HOME")
+			    (get-environment-variable "HOMEPATH")))
+	 (user-conf-dir (nix/win (get-environment-variable "HOME")
+				 (get-environment-variable "APPDATA")))
+	 (user-data-dir (platform-dir-slashes (conc user-conf-dir "/" (nix/win
+								       (conc "." program-filename)
+								       (conc program-filename)))))
+	 (etc-dir (if (eq? (software-type) 'windows)
+		      (get-environment-variable "ALLUSERSPROFILE")
+		      "/etc"))
+         (system-config-file (if* (file-exists? (nix/win (conc etc-dir program-filename ".conf")
+							 (conc etc-dir program-filename ".ini")))
                                   it
                                   #f))
-         (user-config-file (find
-                            (lambda (x) (and (string? x) (file-exists? x)))
-                            (list (get-environment-variable "TEVE_RC")
-                                  (conc user-data-dir "/config")
-                                  (conc home-dir "/." program-filename "rc"))))
-         (cache-dir (conc user-data-dir "/cache"))
+         (user-config-file (platform-dir-slashes
+			    (find
+			     (lambda (x) (and (string? x) (file-exists? x)))
+			     (list (get-environment-variable "TEVE_RC")
+				   (nix/win
+				    (conc user-data-dir "/config")
+				    (conc user-data-dir "/settings.ini"))
+				   (nix/win (conc user-conf-dir "/." program-filename "rc")
+					    (conc user-conf-dir "/" program-filename ".ini"))))))
+         (cache-dir (platform-dir-slashes (conc user-data-dir "/cache")))
          (values `((program-filename . ,program-filename)
                    (home-dir . ,home-dir)
-                   (etc-dir . "/etc")
+                   (etc-dir . ,etc-dir)
                    (user-data-dir . ,user-data-dir)
+		   (user-config-dir . ,user-conf-dir)
                    (user-config-file . ,user-config-file)
                    (system-config-file . ,system-config-file)
                    (cache-dir . ,cache-dir))))
