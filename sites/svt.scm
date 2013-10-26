80d8c5ecd0d70878f51eb368f3eab18cf26d6a01
Jesper Räftegård <jesper@huggpunkt.org>
2013-10-26 16:29:07 +0200

    whitespace

--- sites/svt.scm
+++ sites/svt.scm
@@ -24,17 +24,16 @@
     (let find-first-swf ((stack "")
                          (is-discarding #t)
                          (ch (read-char)))
-      (cond
-       ((eof-object? ch) #f)
-       ((and (char=? #\" ch) (string-prefix? ending stack))
-        (let discard-until-eof ((ch (read-char)))
-          (if (eof-object? ch)
-              (string-reverse stack)
-              (discard-until-eof (read-char)))))
-       (else
-        (find-first-swf (if is-discarding "" (conc ch stack))
-                        (not-if (char=? #\" ch) is-discarding)
-                        (read-char)))))))
+      (cond ((eof-object? ch) #f)
+            ((and (char=? #\" ch) (string-prefix? ending stack))
+             (let discard-until-eof ((ch (read-char)))
+               (if (eof-object? ch)
+                   (string-reverse stack)
+                   (discard-until-eof (read-char)))))
+            (else
+             (find-first-swf (if is-discarding "" (conc ch stack))
+                             (not-if (char=? #\" ch) is-discarding)
+                             (read-char)))))))
 
 (define (svt:swf-player-in url)
   (and-let* ((swf (with-input-from-string (fetch url)
@@ -43,28 +42,26 @@
 
 (define (svt:stream-type-of url bitrate player-type)
   (let ((protocol (url->protocol url)))
-    (cond
-     ((string=? player-type "ios")
-      'hls)
-     ((and (string=? player-type "wmv")
-           (string-prefix? "http" protocol))
-      'http)
-     ((and (string=? player-type "mpeg4")
-           (string=? protocol "rtsp"))
-      'rtsp)
-     ((and (string=? player-type "flash")
-           (string-prefix? "rtmp" protocol))
-      'rtmp)
-     ((and (string=? player-type "flash")
-           (string-prefix? "http" protocol))
-      (if (string-suffix? ".f4m" url)
-          'hds
-          'http))
-     ((and (string=? player-type "wmv")
-           (string=? protocol "mms"))
-      'mms)
-     (else
-      #f))))
+    (cond ((string=? player-type "ios")
+           'hls)
+          ((and (string=? player-type "wmv")
+                (string-prefix? "http" protocol))
+           'http)
+          ((and (string=? player-type "mpeg4")
+                (string=? protocol "rtsp"))
+           'rtsp)
+          ((and (string=? player-type "flash")
+                (string-prefix? "rtmp" protocol))
+           'rtmp)
+          ((and (string=? player-type "flash")
+                (string-prefix? "http" protocol))
+           (if (string-suffix? ".f4m" url)
+               'hds
+               'http))
+          ((and (string=? player-type "wmv")
+                (string=? protocol "mms"))
+           'mms)
+          (else #f))))
 
 (define (svt:json-stream->streams jstr)
   (and-let* ((supplied-url (cdip (assoc "url" jstr)))
@@ -72,17 +69,14 @@
              (player-type (cdip (assoc "playerType" jstr)))
              (stream-type (svt:stream-type-of supplied-url bitrate player-type))
              (url (if (eq? stream-type 'hds)
-                      (conc supplied-url "?hdcore=2.9.4&g="
-                            (string-upcase (make-rnd-string 12)))
+                      (conc supplied-url "?hdcore=2.9.4&g=" (string-upcase (make-rnd-string 12)))
                       supplied-url)))
     (case stream-type
-      ((hls)
-       (hls-master->streams url))
-      (else
-       (list (make-stream (make-stream-value 'url url)
-                          (make-stream-value 'stream-type stream-type)
-                          (if (< 0 bitrate)
-                              (make-stream-value 'bitrate bitrate))))))))
+      ((hls) (hls-master->streams url))
+      (else (list (make-stream (make-stream-value 'url url)
+                               (make-stream-value 'stream-type stream-type)
+                               (if (< 0 bitrate)
+                                   (make-stream-value 'bitrate bitrate))))))))
 
 ;;; Return a list of videos constructed from the json object
 ;;; downloaded from the supplied url, or return #f.
@@ -95,33 +89,25 @@
            (video-id (json-ref json-data "videoId"))
            (popout-path (json-ref json-data "context" "popoutUrl"))
            (is-live (json-ref json-data "video" "live"))
-           (play-path (cond
-                      ((not popout-path) #f)
-                      ((string-prefix? "http" popout-path) popout-path)
-                      (else
-                       (conc base-path popout-path))))
+           (play-path (cond ((not popout-path) #f)
+                            ((string-prefix? "http" popout-path) popout-path)
+                            (else (conc base-path popout-path))))
            (swf-player (delay (svt:swf-player-in play-path)))
            (add-video-values (lambda (stream)
                                (update-stream
                                 stream
-                                (make-stream-value 'default-filename
-                                                   (conc "svt-video-" video-id))
+                                (make-stream-value 'default-filename (conc "svt-video-" video-id))
                                 (make-stream-value 'subtitles subtitles)
                                 (make-stream-value 'view-at play-path)
                                 (make-stream-value 'live is-live)
                                 (if (eq? 'hls (stream-ref 'stream-type stream))
-                                    (make-stream-value 'ffmpeg-parameters
-                                                       "-bsf:a aac_adtstoasc"))
+                                    (make-stream-value 'ffmpeg-parameters "-bsf:a aac_adtstoasc"))
                                 (if (eq? 'rtmp (stream-ref 'stream-type stream))
-                                    (make-stream-value 'swf-player
-                                                       (force swf-player)))))))
-      (list
-       (streams->video
-        (fold (lambda (objs streams)
-                (append (filter-map add-video-values objs)
-                        streams))
-              '()
-              (filter-map svt:json-stream->streams references)))))))
+                                    (make-stream-value 'swf-player (force swf-player)))))))
+      (list (streams->video (fold (lambda (objs streams)
+                                    (append (filter-map add-video-values objs) streams))
+                                  '()
+                                  (filter-map svt:json-stream->streams references)))))))
 
 ;;; Return a JSON url extracted from an embedded svtplay video, or
 ;;; return #f.
@@ -142,15 +128,13 @@
         (string-contains url "://www.öppetarkiv.se/")
         (string-contains url "://xn--ppetarkiv-z7a.se/")
         (string-contains url "://www.xn--ppetarkiv-z7a.se/"))
-    (filter video? (svt:json-url->videos
-                    (add-http-get-query-var url "output" "json"))))
+    (filter video? (svt:json-url->videos (add-http-get-query-var url "output" "json"))))
    ((or (string-contains url "://www.svt.se/"))
     (and-let* ((json-url (svt:embedded-player->json-url url)))
       (filter video?
               (svt:json-url->videos (if (string-prefix? "http" json-url)
                                         json-url
-                                        (conc (uri->base-path url)
-                                              json-url))))))
+                                        (conc (uri->base-path url) json-url))))))
    (else
     #f)))
 
