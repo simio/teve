simio/teve

commit 367608978441908630cf224de18b0a51b17e563d

Add preset channel names For example, "teve aktuellt" will expand into "teve http://www.svtplay.se/aktuellt".
Though undocumented and untested as of yet, channels may be added and
modified in teve.conf.

config.scm
@@ -32,24 +32,32 @@
 ;;;   (cfg 'branch 'leaf)		=> 5	; it's now set
 (define make-configuration
   (lambda sources
-    (let* ((default-configuration '((operators (play? . #f)
-                                               (download? . #f)
-                                               (list? . #f)
-                                               (stream-id . #f)
-                                               (video-id . #f)
-                                               (output-filename . #f))
-                                    (external-programs (rtmpdump . "rtmpdump")
-                                                       (ffmpeg . "ffmpeg")
-                                                       (mplayer . "mplayer")
-                                                       (curl . "curl")
-                                                       (php . "php")
-                                                       (adobehds.php . "gpl/AdobeHDS.php"))
-                                    (preferences (default-action . download)
-                                                 (ideal-bitrate . 2500)
-                                                 (ideal-pixel-width . 1280)
-                                                 (use-cache . #t)
-                                                 (cache-default-ttl . 300)
-                                                 (cache-override-ttl . #f))))
+    (let* ((default-configuration
+             '((operators (play? . #f)
+                          (download? . #f)
+                          (list? . #f)
+                          (stream-id . #f)
+                          (video-id . #f)
+                          (output-filename . #f))
+               (external-programs (rtmpdump . "rtmpdump")
+                                  (ffmpeg . "ffmpeg")
+                                  (mplayer . "mplayer")
+                                  (curl . "curl")
+                                  (php . "php")
+                                  (adobehds.php . "gpl/AdobeHDS.php"))
+               (preferences (default-action . download)
+                            (ideal-bitrate . 2500)
+                            (ideal-pixel-width . 1280)
+                            (use-cache . #t)
+                            (cache-default-ttl . 300)
+                            (cache-override-ttl . #f))
+               (channels ("aktuellt" . "http://www.svtplay.se/aktuellt")
+                         ("rapport" . "http://www.svtplay.se/rapport")
+                         ("svt1" . "http://www.svtplay.se/kanaler/svt1")
+                         ("svt2" . "http://www.svtplay.se/kanaler/svt2")
+                         ("barnkanalen" . "http://www.svtplay.se/kanaler/barnkanalen")
+                         ("svt24" . "http://www.svtplay.se/kanaler/svt24")
+                         ("kunskapskanalen" . "http://www.svtplay.se/kanaler/kunskapskanalen"))))
            (values (apply atree-merge (reverse (cons default-configuration sources)))))
       (lambda args
         (cond ((null? args) values)

uri2vid.scm
@@ -14,7 +14,7 @@
 
 (module uri->video (uri->videos)
 (import scheme chicken srfi-1 data-structures
-        misc-helpers svt tv4)
+        misc-helpers config svt tv4)
 
 ;;; The scrape-list is a list of all uri->video procedures
 (define *scraper-list* '())
@@ -32,11 +32,17 @@
 (add-scraper svt:uri->videos)
 (add-scraper tv4:uri->videos)
 
+;;; Expand preset strings into uri:s
+(define (expand-channels str)
+  (or (*cfg* 'channels str) str))
+
 ;;; Dispatcher
 ;;; Each uri->videos procedure is applied to the uri. The resulting
 ;;; videos are appended into a video-list, which is returned.
 (define (uri->videos uri)
-  (delete-duplicates (apply append (filter-map (lambda (u->v) (u->v (prepend-default-protocol uri)))
-                                               *scraper-list*))))
+  (delete-duplicates
+   (apply append (filter-map (lambda (u->v)
+                               (u->v (prepend-default-protocol (expand-channels uri))))
+                             *scraper-list*))))
 
 )

commit 36402dcf70e2c285549305e493e4b8cd59d48755

Don't use strings for keys in configuration (Fixes problem introduced in 367608978441908630cf224de18b0a51b17e563d)

config.scm
@@ -51,13 +51,13 @@
                             (use-cache . #t)
                             (cache-default-ttl . 300)
                             (cache-override-ttl . #f))
-               (channels ("aktuellt" . "http://www.svtplay.se/aktuellt")
-                         ("rapport" . "http://www.svtplay.se/rapport")
-                         ("svt1" . "http://www.svtplay.se/kanaler/svt1")
-                         ("svt2" . "http://www.svtplay.se/kanaler/svt2")
-                         ("barnkanalen" . "http://www.svtplay.se/kanaler/barnkanalen")
-                         ("svt24" . "http://www.svtplay.se/kanaler/svt24")
-                         ("kunskapskanalen" . "http://www.svtplay.se/kanaler/kunskapskanalen"))))
+               (channels (aktuellt . "http://www.svtplay.se/aktuellt")
+                         (rapport . "http://www.svtplay.se/rapport")
+                         (svt1 . "http://www.svtplay.se/kanaler/svt1")
+                         (svt2 . "http://www.svtplay.se/kanaler/svt2")
+                         (barnkanalen . "http://www.svtplay.se/kanaler/barnkanalen")
+                         (svt24 . "http://www.svtplay.se/kanaler/svt24")
+                         (kunskapskanalen . "http://www.svtplay.se/kanaler/kunskapskanalen"))))
            (values (apply atree-merge (reverse (cons default-configuration sources)))))
       (lambda args
         (cond ((null? args) values)

uri2vid.scm
@@ -34,7 +34,7 @@
 
 ;;; Expand preset strings into uri:s
 (define (expand-channels str)
-  (or (*cfg* 'channels str) str))
+  (or (*cfg* 'channels (string->symbol str)) str))
 
 ;;; Dispatcher
 ;;; Each uri->videos procedure is applied to the uri. The resulting